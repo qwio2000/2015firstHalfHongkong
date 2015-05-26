7c4b2057841bd47d8c2a36ad5c0ae307c8cc19bf
mashisoon <mashisoon@jeione.com>
2015-05-26 17:15:52 +0900

    커먼생성

---
+++ src/main/java/com/jeiglobal/hk/service/CommonService.java
@@ -0,0 +1,31 @@
+package com.jeiglobal.hk.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jeiglobal.hk.domain.common.AuthMemberInfo;
+import com.jeiglobal.hk.repository.CommonRepository;
+
+@Service
+public class CommonService {
+	
+	@Autowired
+	private CommonRepository commonRepository;
+	
+	public List<String> getHuheiDayList(AuthMemberInfo authMemberInfo) {
+		// TODO Auto-generated method stub
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("jisa", authMemberInfo.getJisaCD());
+		map.put("empKeyLvCD", authMemberInfo.getEmpKeyLvCD());
+		map.put("yyyy", sdf.format(cal.getTime()).substring(0, 4));
+		map.put("mm", sdf.format(cal.getTime()).substring(5));
+		return commonRepository.findDateSetting(map);
+	}
+}
